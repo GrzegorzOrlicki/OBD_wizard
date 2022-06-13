4782c285081188f20d4ed990e0a6f1a99986844e
Grzegorz Orlicki <grzorl2907@gmail.com>
2022-06-13 20:31:31 +0200

    Source code init

---
+++ client_app/src/client.py
@@ -0,0 +1,7 @@
+import obd_wizard
+
+def main():
+    print("Client_app")
+
+if __name__ == "__main__":
+    main()
---
+++ server_app/src/server.py
@@ -0,0 +1,7 @@
+import obd_wizard
+
+def main():
+    print("Server_app")
+
+if __name__ == "__main__":
+    main()
